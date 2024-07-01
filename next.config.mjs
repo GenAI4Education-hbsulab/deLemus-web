5c9b6fc59c79dcb7f5c265fe9c1299977f9a51d2
hans2001 <hans20010907@gmail.com>
2024-07-01 21:08:36 +0800

    fix:add helper files

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   experimental: {
     typedRoutes: true,
+      middleware: false,
   },
     images: {
       domains: ['via.placeholder.com'],
