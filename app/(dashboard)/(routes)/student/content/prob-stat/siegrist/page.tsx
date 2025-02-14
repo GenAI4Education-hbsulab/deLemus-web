edd7f2a66ebceb951f892ca5ad3de07e9dfe34a1
YuetAu <sam07@live.hk>
2025-02-14 15:36:52 +0800

    fix and deploy

--- app/(dashboard)/(routes)/student/content/prob-stat/siegrist/page.tsx
+++ app/(dashboard)/(routes)/student/content/prob-stat/siegrist/page.tsx
@@ -1,14 +1,14 @@
 export default function Siegrist() {
-    return (
+  return (
     <div
       className="relative"
       style={{ paddingBottom: "56.25%", height: 0, overflow: "hidden" }}
     >
-    <iframe
+      <iframe
         src="https://stats.libretexts.org/Bookshelves/Probability_Theory/Probability_Mathematical_Statistics_and_Stochastic_Processes_(Siegrist)"
         className="absolute top-0 left-0 w-full h-full"
         frameBorder="0"
-    ></iframe>
+      ></iframe>
     </div>
-    );
+  );
 }
